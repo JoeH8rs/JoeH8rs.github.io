6826670830bb1f9134e58942f968133a71b403dd
Joe Neuman <Joe Neuman>
2016-01-01 22:36:56 -0700

    added w3d5

--- br/js/cut.js
+++ br/js/cut.js
@@ -1,37 +1,55 @@
-$(document).ready(function() {
-	try {
-		$('body').ripples({
-			resolution: 512,
-			dropRadius: 20, //px
-			perturbance: 0.04,
-		});
-		
-	}
-	catch (e) {
-		$('.error').show().text(e);
-	}
+$(document).ready(function () {
+    try {
+        $('body').ripples({
+            resolution: 512,
+            dropRadius: 20, //px
+            perturbance: 0.05,
+        });
 
-	$('.js-ripples-disable').on('click', function() {
-		$('body, body').ripples('destroy');
-		$(this).hide();
-	});
+    }
+    catch (e) {
+        $('.error').show().text(e);
+    }
 
-	$('.js-ripples-play').on('click', function() {
-		$('body, body').ripples('play');
-	});
+    $('.js-ripples-disable').on('click', function () {
+        $('body, body').ripples('destroy');
+        $(this).hide();
+    });
 
-	$('.js-ripples-pause').on('click', function() {
-		$('body, body').ripples('pause');
-	});
+    $('.js-ripples-play').on('click', function () {
+        $('body, body').ripples('play');
+    });
 
-	// Automatic drops
-	setInterval(function() {
-		var $el = $('body');
-		var x = Math.random() * $el.outerWidth()
-		var y = Math.random() * $el.outerHeight();
-		var dropRadius = 20;
-		var strength = 0.01 + Math.random() * 0.04;
+    $('.js-ripples-pause').on('click', function () {
+        $('body, body').ripples('pause');
+    });
 
-		$el.ripples('drop', x, y, dropRadius, strength);
-	}, 900);
+    // Automatic drops
+    setInterval(function () {
+        var $el = $('body');
+        var x = Math.random() * $el.outerWidth()
+        var y = Math.random() * $el.outerHeight();
+        var dropRadius = 40;
+        var strength = .05 + Math.random() * .001;
+
+        $el.ripples('drop', x, y, dropRadius, strength);
+    }, 2000);
+
+    $(".search").click(function () {
+        $(".searchArea").animate({ "top": "-220px" }, "slow");
+    });
+
+    $(".search").click(function () {
+        $(".someText").hide(0).delay(800).fadeIn();
+    });
+$(document).ready(function () {
+    setTimeout(function () {
+        
+            $(".backfade").animate({
+                "opacity": "0.86"
+            }, 12000);
+        }, 6000);
+
+    });
+
 });
